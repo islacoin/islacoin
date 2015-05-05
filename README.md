e32eee184092ecdffe92f6a466d8debfe31456c0
isladev <islacoindev@gmail.com>
2015-05-05 12:52:25 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Specifications:
+- Algo: X11
+- Block time: 60 seconds
+- Max Supply: 35129400 
+- Block reward: 75-95
+- PoW Supply: 3512940 
+- Last PoW block: 44640
+- PoS Interest: 12% per year
+- RPC Port: 9831
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define IslaCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "IslaCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
