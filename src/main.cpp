a20544c5215da2d3b81e4861453c7c6068389876
IslaCoin <dev@islacoin.net>
2015-07-12 22:20:58 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2116,6 +2116,10 @@ bool CBlock::AcceptBlock()
     CBlockIndex* pindexPrev = (*mi).second;
     int nHeight = pindexPrev->nHeight+1;
         
+    if (IsProofOfWork() && nHeight > FORK_BLOCK) {
+        return DoS(100, error("AcceptBlock() : reject proof-of-work at height %d", nHeight));
+    }
+    
     if (IsProofOfStake() && nHeight < MODIFIER_INTERVAL_SWITCH)
         return DoS(100, error("AcceptBlock() : reject proof-of-stake at height %d", nHeight));
 
